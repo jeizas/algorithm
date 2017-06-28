8911bbdb06712310d9e462bbd4587d24aa030b19
jeizas <jeizas1210@gmail.com>
2017-06-28 23:44:57 +0800

    new file:   src/com/jeizas/test/Main.java

---
+++ src/com/jeizas/test/Main.java
@@ -0,0 +1,19 @@
+package com.jeizas.test;
+
+/**
+ * Created by jeizas on 6/27/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        int[] a = {1, 2, 3};
+//
+//        for(int p : a) {
+//            System.out.print(p + " ");
+//        }
+        final StringBuffer str = new StringBuffer("999");
+        str.append("000");
+
+        System.out.print(str.toString());
+    }
+}
